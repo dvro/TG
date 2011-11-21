696c5bbf8ff2bb53d1dd6f7fe3aa90a2ed9562d3
Dayvid Victor <victor.dvro@gmail.com>
2011-11-21 18:15:17 -0300

    artificial_databases created

--- artificial_database/gen.py
+++ artificial_database/gen.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
         for j in range (0,45):
             print '%d, %d, %d' % (i, j, 0)
 
-    for i in [k + 0.5 for k in range(15,25)]:
-        for j in range (0, 10):
+    for i in [k + 0.5 for k in range(5, 15)]:
+        for j in range (0, 5):
             print '%.2f, %d, %d' % (i, j, 1)
